eaa701ed9f7ae825140be795d0bcd40d2dbae29c
Austin Frey <austin.frey@therapynotes.com>
2023-09-07 13:37:46 -0600

    initial commit

---
+++ app/mock-zaraz.tsx
@@ -0,0 +1,49 @@
+type Purpose = {
+  [key: string]: boolean;
+};
+
+function MockZaraz(initialPurposes: Purpose[] = []) {
+  const PURPOSES: { [key: string]: boolean } = {};
+
+  initialPurposes.map((p) => {
+    const purposeId = Object.keys(p)[0];
+    PURPOSES[purposeId] = p[purposeId];
+  });
+
+  function get(purposeId: string): boolean {
+    console.log("MOCK get", purposeId, PURPOSES[purposeId]);
+    return PURPOSES[purposeId];
+  }
+
+  function set(consentPreferences: Purpose) {
+    const purposeId = Object.keys(consentPreferences)[0];
+    PURPOSES[purposeId] = consentPreferences[purposeId];
+    console.log("MOCK set", consentPreferences);
+  }
+
+  function getAll() {
+    console.log("MOCK getAll", PURPOSES);
+    return PURPOSES;
+  }
+
+  function setAll(consentStatus: boolean) {
+    const purposeIds = Object.keys(PURPOSES);
+    purposeIds.map((id) => {
+      PURPOSES[id] = consentStatus;
+    });
+    console.log("MOCK setAll", consentStatus, PURPOSES);
+  }
+
+  const consent = {
+    get,
+    set,
+    getAll,
+    setAll,
+  };
+
+  return {
+    consent,
+  };
+}
+
+export default MockZaraz;
---
+++ app/zaraz-provider.tsx
@@ -0,0 +1,52 @@
+import { ReactNode, useEffect, useState } from "react";
+import MockZaraz from "./mock-zaraz";
+
+declare const zaraz: any;
+
+type Props = {
+  children: ReactNode;
+};
+
+const mockZaraz =
+  typeof zaraz === "undefined" ? MockZaraz([{ "test-purpose": false }]) : zaraz;
+
+const ZarazProvider = ({ children }: Props) => {
+  const [contentApiReady, setContentApiReady] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setShowModal(!mockZaraz.consent.get("test-purpose"));
+
+    document.addEventListener("zarazConsentAPIReady", () => {
+      console.log("Zaraz Content API ready");
+      setContentApiReady(true);
+    });
+  }, [showModal, contentApiReady]);
+
+  const Modal = () => {
+    return (
+      <div className="zaraz-modal">
+        <p>
+          We use cookies and other tracking technologies, including session
+          replay tracking, to improve your experience, to provide and remember
+          your login, to personalize content and ads, to record sessions for
+          later playback, and to optimize site functionality.
+        </p>
+
+        <button
+          className="approve-button"
+          onClick={() => {
+            mockZaraz.consent.set({ "test-purpose": true });
+            setShowModal(false);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+    );
+  };
+
+  return showModal && contentApiReady ? <Modal></Modal> : <>{children}</>;
+};
+
+export default ZarazProvider;
