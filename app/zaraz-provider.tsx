be9b416a48e080c057b97aee3130058e8d6d9161
Austin Frey <austin.frey@therapynotes.com>
2023-09-08 10:13:26 -0600

    debug log

--- app/zaraz-provider.tsx
+++ app/zaraz-provider.tsx
@@ -56,7 +56,7 @@ const ZarazProvider = ({ children }: Props) => {
             type="checkbox"
             id="marketingCheckbox"
             name="marketingCheckbox"
-            value={purposes[1][0]}
+            value={JSON.stringify(purposes[1])}
           />
         </div>
         <div>
@@ -68,7 +68,7 @@ const ZarazProvider = ({ children }: Props) => {
             type="checkbox"
             id="trackingCheckbox"
             name="trackingCheckbox"
-            value={purposes[2][0]}
+            value={JSON.stringify(purposes[2])}
           />
         </div>
 
